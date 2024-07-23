17a572696df788a30773c0d9dfa710ca8cfb7643
bluvan96 <batonchik.2003@Mail.ru>
2024-07-23 17:41:19 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print('1st program')
+print(5 * 9 ** 0.5)
+print('2nd program')
+print(9.99 > 9.98 and 1000 != 1000.1)
+print('3rd program')
+print(1234 % 1000 // 10 + 5678 % 1000 // 10)
+print('4th program')
+print(int(13.42) == int(42.13 % 1 * 100))
+
